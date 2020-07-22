ef80ce06c9826418e5dc9247858f63f57707ef52
Snower87 <55270945+Snower87@users.noreply.github.com>
2020-07-22 18:16:39 +0500

    2. Добавил диалоговое окно выбора файлов JFileChoser.

--- src/main/java/ru/begletsov/converter/GUIConv.java
+++ src/main/java/ru/begletsov/converter/GUIConv.java
@@ -1,10 +1,13 @@
 package ru.begletsov.converter;
 
 import javax.swing.*;
+import javax.swing.filechooser.FileSystemView;
 import java.awt.*;
+import java.io.File;
 
 public class GUIConv extends JFrame {
     private JLabel text1;
+    private JFileChooser fileChooser;
 
     public GUIConv() {
         super("Заголовок окна");
@@ -12,5 +15,14 @@ public class GUIConv extends JFrame {
         text1 = new JLabel("Пробный текст");
         text1.setToolTipText("Просто так");
         add(text1);
+
+        fileChooser = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+        int returnValue = fileChooser.showOpenDialog(null);
+        if (returnValue == JFileChooser.APPROVE_OPTION) {
+            File file = fileChooser.getSelectedFile();
+            //какие-то действия
+            int aaa = 0;
+        }
+
     }
 }

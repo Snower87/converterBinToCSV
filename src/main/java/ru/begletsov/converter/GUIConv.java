1906f780688018d4c1158f0b91ad1f43e76d5dc3
Snower87 <55270945+Snower87@users.noreply.github.com>
2020-07-28 16:24:58 +0500

    3. Добавил кнопку buttonOpenFileCloser и привязал к ней диалоговое окно выбора файлов JFileChoser при нажатии.

--- src/main/java/ru/begletsov/converter/GUIConv.java
+++ src/main/java/ru/begletsov/converter/GUIConv.java
@@ -3,26 +3,57 @@ package ru.begletsov.converter;
 import javax.swing.*;
 import javax.swing.filechooser.FileSystemView;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 
 public class GUIConv extends JFrame {
     private JLabel text1;
     private JFileChooser fileChooser;
+    private JButton buttonOpenFileChoser;
+    private File[] selectedFiles;
+    private File directory;
 
     public GUIConv() {
-        super("Заголовок окна");
+        super("Конвертер Bin -> CSV файлов");
         setLayout(new FlowLayout());
         text1 = new JLabel("Пробный текст");
         text1.setToolTipText("Просто так");
+        buttonOpenFileChoser = new JButton("Выберите файлы");
+        add(buttonOpenFileChoser);
         add(text1);
 
-        fileChooser = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
-        int returnValue = fileChooser.showOpenDialog(null);
-        if (returnValue == JFileChooser.APPROVE_OPTION) {
-            File file = fileChooser.getSelectedFile();
-            //какие-то действия
-            int aaa = 0;
-        }
+        //fileChooser = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+        fileChooser = new JFileChooser();
 
+        fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+        fileChooser.setFileHidingEnabled(false);
+        fileChooser.setMultiSelectionEnabled(true);
+
+        buttonOpenFileChoser.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                switch (fileChooser.showDialog(null, "Открыть")) {
+                    case  JFileChooser.APPROVE_OPTION:
+                        selectedFiles = fileChooser.getSelectedFiles();
+                        directory = fileChooser.getCurrentDirectory();
+                        break;
+                    case JFileChooser.CANCEL_OPTION:
+                        int aaa = 10;
+                        break;
+                    default:
+                        break;
+                }
+
+
+            }
+        });
+
+
+
+
+
+        int asdasd = 0;
+        asdasd++;
     }
 }

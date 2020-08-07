a7265ee33dc14072162a4e4e928c6a25a9435aa3
Snower87 <55270945+Snower87@users.noreply.github.com>
2020-08-07 08:33:25 +0500

    9. Добавил класс-компонент CheckBoxList.

---
+++ src/main/java/ru/begletsov/component/CheckBoxList.java
@@ -0,0 +1,67 @@
+package ru.begletsov.component;
+
+import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+/**
+ * Компонент CheckBoxList на базе JList
+ * 1) создание класса
+ * @author Sergei Begletsov
+ * @since 07.08.2020
+ * @version 1
+ */
+
+public class CheckBoxList extends JList {
+    protected static Border noFocusBorder =
+            new EmptyBorder(1, 1, 1, 1);
+
+    public CheckBoxList()
+    {
+        setCellRenderer(new CellRenderer());
+
+        addMouseListener(new MouseAdapter()
+                         {
+                             public void mousePressed(MouseEvent e)
+                             {
+                                 int index = locationToIndex(e.getPoint());
+
+                                 if (index != -1) {
+                                     JCheckBox checkbox = (JCheckBox)
+                                             getModel().getElementAt(index);
+                                     checkbox.setSelected(
+                                             !checkbox.isSelected());
+                                     repaint();
+                                 }
+                             }
+                         }
+        );
+
+        setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+    }
+
+    protected class CellRenderer implements ListCellRenderer
+    {
+        public Component getListCellRendererComponent(
+                JList list, Object value, int index,
+                boolean isSelected, boolean cellHasFocus)
+        {
+            JCheckBox checkbox = (JCheckBox) value;
+            checkbox.setBackground(isSelected ?
+                    getSelectionBackground() : getBackground());
+            checkbox.setForeground(isSelected ?
+                    getSelectionForeground() : getForeground());
+            checkbox.setEnabled(isEnabled());
+            checkbox.setFont(getFont());
+            checkbox.setFocusPainted(false);
+            checkbox.setBorderPainted(true);
+            checkbox.setBorder(isSelected ?
+                    UIManager.getBorder(
+                            "List.focusCellHighlightBorder") : noFocusBorder);
+            return checkbox;
+        }
+    }
+}

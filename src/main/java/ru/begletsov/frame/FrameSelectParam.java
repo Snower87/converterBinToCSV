313c5dddb19161d4529447d5d49fdd5ff68880b3
Snower87 <55270945+Snower87@users.noreply.github.com>
2020-08-10 13:00:24 +0500

    14. Чистка проекта, добавил новые параметры для абонентов.

--- src/main/java/ru/begletsov/frame/FrameSelectParam.java
+++ src/main/java/ru/begletsov/frame/FrameSelectParam.java
@@ -17,15 +17,14 @@ import java.util.stream.Collectors;
 /**
  * Класс FrameSelectParam - окно выбора параметров
  * 1) создание класса 2) добавил объект класса Service со списком абонентов + методы добавляющие абоненты и их пар-ры
- * 3) добавил списки listCheckBox для каждого абонента: BVKsh, BPS_UVKE_SM, KI, J1939
+ * 3) добавил списки listCheckBox для каждого абонента: BVKsh, BPS_UVKE_SM, KI, J1939 4) чистка проекта, добавил новые параметры для абонентов
  * @author Sergei Begletsov
  * @since 07.08.2020
- * @version 2
+ * @version 4
  */
 
 public class FrameSelectParam extends JFrame {
     private JTabbedPane tabbedPane;
-    private List<JCheckBox> listCheckBox;
     private List<JCheckBox> listCheckBox_BVKsh;
     private List<JCheckBox> listCheckBox_BPS_UVKE_SM;
     private List<JCheckBox> listCheckBox_KI;
@@ -47,7 +46,6 @@ public class FrameSelectParam extends JFrame {
         setSize(400, 400);
         setLocationByPlatform(true);
 
-        listCheckBox = new ArrayList<>();
         listCheckBox_BVKsh = new ArrayList<>();
         listCheckBox_BPS_UVKE_SM = new ArrayList<>();
         listCheckBox_KI = new ArrayList<>();
@@ -56,22 +54,6 @@ public class FrameSelectParam extends JFrame {
         serviceAbonentsAndParam = new Service();
         this.addAbonentsRS485();
         this.addParamRS485();
-        JCheckBox check1 = new JCheckBox("One");
-        JCheckBox check2 = new JCheckBox("Two");
-        JCheckBox check3 = new JCheckBox("3");
-        JCheckBox check4 = new JCheckBox("4");
-        JCheckBox check5 = new JCheckBox("5");
-        JCheckBox check6 = new JCheckBox("6");
-        JCheckBox check7 = new JCheckBox("7");
-        JCheckBox check8 = new JCheckBox("8");
-        JCheckBox check9 = new JCheckBox("9");
-        JCheckBox check10 = new JCheckBox("10");
-        JCheckBox[] myList = { check1, check2, check3, check4, check5, check6, check7, check8, check9, check10};
-        //JList[] myList = { check1, check2, check3, check4, check5, check6};
-
-        //listCheckBox = (Vector) Arrays.asList(myList);
-        listCheckBox = Arrays.asList(myList);
-        //cbList.setListData((Vector) listCheckBox);
 
         //Добавление абонентов и их пар-ов на панель выбора
         tabbedPane = new JTabbedPane(JTabbedPane.LEFT);
@@ -92,10 +74,16 @@ public class FrameSelectParam extends JFrame {
 
                 switch (nameObmen) {
                     case "BR_BVKsh":
-                    case "BR_KI":
-                    case "BR_J1939":
-                    case "BR_BPS_UVKE_SM":
                         listCheckBox_BVKsh = Arrays.asList(massCheckBoxes);
+                        break;
+                    case "BR_KI":
+                        listCheckBox_KI = Arrays.asList(massCheckBoxes);
+                        break;
+                    case "BR_J1939":
+                        listCheckBox_J1939 = Arrays.asList(massCheckBoxes);
+                        break;
+                    case "BR_BPS_UVKE_SM":
+                        listCheckBox_BPS_UVKE_SM = Arrays.asList(massCheckBoxes);
                         break;
                 }
                 tabbedPane.addTab(nameObmen, checkBoxListCurrAbonent);
@@ -111,8 +99,6 @@ public class FrameSelectParam extends JFrame {
 
             @Override
             public void windowClosing(WindowEvent e) {
-                listChoseParam.setListChoseParam_UVKE_SM(getChoseUserParam(listCheckBox));
-
                 listChoseParam.setListChoseParam_BVKsh(getChoseUserParam(listCheckBox_BVKsh));
                 listChoseParam.setListChoseParam_UVKE_SM(getChoseUserParam(listCheckBox_BPS_UVKE_SM));
                 listChoseParam.setListChoseParam_KI(getChoseUserParam(listCheckBox_KI));
@@ -149,6 +135,7 @@ public class FrameSelectParam extends JFrame {
     public List getChoseUserParam(List<JCheckBox> listCheckBox) {
         List rsl = new ArrayList();
         for (int ind = 0; ind < listCheckBox.size(); ind++) {
+            boolean asdas  = listCheckBox.get(ind).isSelected();
             if (listCheckBox.get(ind).isSelected()) {
                 //rsl.add(ind);
                 rsl.add(listCheckBox.get(ind).getText());
@@ -176,12 +163,25 @@ public class FrameSelectParam extends JFrame {
         serviceAbonentsAndParam.addParam("BR_BVKsh", new Param((byte)0x01, (byte)0x02, (short) 3, "REQ", "BR_BVKsh", "Управление величиной открытия клапана YP1", "YP1", "ANLG2", (byte)0, 1, 1, 1));
         serviceAbonentsAndParam.addParam("BR_BVKsh", new Param((byte)0x01, (byte)0x02, (short) 4, "REQ", "BR_BVKsh", "Управление величиной открытия клапана YP2", "YP2", "ANLG2", (byte)0, 1, 1, 1));
         serviceAbonentsAndParam.addParam("BR_BVKsh", new Param((byte)0x01, (byte)0x02, (short) 5, "REQ", "BR_BVKsh", "Управление величиной открытия клапана YP3", "YP3", "ANLG2", (byte)0, 1, 1, 1));
+        serviceAbonentsAndParam.addParam("BR_BVKsh", new Param((byte)0x01, (byte)0x02, (short) 6, "REQ", "BR_BVKsh", "Управление величиной открытия клапана YP4", "YP4", "ANLG2", (byte)0, 1, 1, 1));
+        serviceAbonentsAndParam.addParam("BR_BVKsh", new Param((byte)0x01, (byte)0x02, (short) 7, "REQ", "BR_BVKsh", "Управление величиной открытия клапана YP5", "YP5", "ANLG2", (byte)0, 1, 1, 1));
+        serviceAbonentsAndParam.addParam("BR_BVKsh", new Param((byte)0x01, (byte)0x02, (short) 8, "REQ", "BR_BVKsh", "Управление величиной открытия клапана YP6", "YP6", "ANLG2", (byte)0, 1, 1, 1));
+        serviceAbonentsAndParam.addParam("BR_BVKsh", new Param((byte)0x01, (byte)0x02, (short) 9, "REQ", "BR_BVKsh", "Управление величиной открытия клапана YP7", "YP7", "ANLG2", (byte)0, 1, 1, 1));
 
         serviceAbonentsAndParam.addParam("BR_BPS_UVKE_SM", new Param((byte)0x01, (byte)0x03, (short) 47, "REQ", "BR_BPS_UVKE_SM", "Температура в кабине машиниста", "Temp_cabin_SMK", "ANLG2", (byte)0, 1, 1, 1));
+        serviceAbonentsAndParam.addParam("BR_BPS_UVKE_SM", new Param((byte)0x01, (byte)0x03, (short) 24, "REQ", "BR_BPS_UVKE_SM", "Скорость платформы от датчика 1.1", "speed_PP1", "ANLG1", (byte)1, 0.01f, 0, 2));
+        serviceAbonentsAndParam.addParam("BR_BPS_UVKE_SM", new Param((byte)0x01, (byte)0x03, (short) 26, "REQ", "BR_BPS_UVKE_SM", "Скорость платформы от датчика 1.2", "speed_PP2", "ANLG1", (byte)1, 0.01f, 0, 2));
+        serviceAbonentsAndParam.addParam("BR_BPS_UVKE_SM", new Param((byte)0x01, (byte)0x03, (short) 28, "REQ", "BR_BPS_UVKE_SM", "Угол поворота платформы от датчика 1.1", "angle_PP1", "ANLG1", (byte)0, 0.1f, 0, 2));
+        serviceAbonentsAndParam.addParam("BR_BPS_UVKE_SM", new Param((byte)0x01, (byte)0x03, (short) 30, "REQ", "BR_BPS_UVKE_SM", "Угол поворота платформы от датчика 1.2", "angle_PP2", "ANLG1", (byte)0, 0.1f, 0, 2));
+        serviceAbonentsAndParam.addParam("BR_BPS_UVKE_SM", new Param((byte)0x01, (byte)0x03, (short) 50, "REQ", "BR_BPS_UVKE_SM", "Неисправность 1", "fault_1_SMK_hex", "ANLG1", (byte)0, 1, 0, 2));
 
         serviceAbonentsAndParam.addParam("BR_KI", new Param((byte)0x01, (byte)0x04, (short) 41, "REQ", "BR_KI", "Давление ГМ в пневмогидроаккумуляторе АК3", "DI7.2_KI3", "ANLG1", (byte)0, 0.01f, 0, 2));
+        serviceAbonentsAndParam.addParam("BR_KI", new Param((byte)0x01, (byte)0x04, (short) 3, "REQ", "BR_KI", "Напряжение бортовой сети", "U_bs_KI1", "ANLG1", (byte)0, 0.5f, 0, 2));
+        serviceAbonentsAndParam.addParam("BR_KI", new Param((byte)0x01, (byte)0x04, (short) 5, "REQ", "BR_KI", "Изменение физических свойств гидравлического масла", "Ch_fiz_oil_KI1", "ANLG1", (byte)0, 0.01f, 0, 2));
 
         serviceAbonentsAndParam.addParam("BR_J1939", new Param((byte)0x02, (byte)0x01, (short) 3, "REQ", "BR_J1939", "Уставка по частоте вращения дизеля", "n_Diezel_ust", "ANLG1", (byte)0, 0.125f, 0, 2));
+        serviceAbonentsAndParam.addParam("BR_J1939", new Param((byte)0x02, (byte)0x01, (short) 5, "REQ", "BR_J1939", "Уставка по моменту дизеля", "M_Diezel_ust", "ANLG2", (byte)0, 1, 1, 1));
         serviceAbonentsAndParam.addParam("BR_J1939", new Param((byte)0x02, (byte)0x01, (short) 9, "REQ", "BR_J1939", "Текущая скорость", "Engine_Speed", "ANLG1", (byte)0, 0.125f, 0, 2));
+        serviceAbonentsAndParam.addParam("BR_J1939", new Param((byte)0x02, (byte)0x01, (short) 54, "REQ", "BR_J1939", "Моточасы", "Моточасы", "ANLG1", (byte)0, 0.05f, 0, 4));
     }
 }

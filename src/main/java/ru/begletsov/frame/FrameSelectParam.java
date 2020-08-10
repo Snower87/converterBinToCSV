852299396b2dd76848c284c2b366b6718714534c
Snower87 <55270945+Snower87@users.noreply.github.com>
2020-08-10 12:16:01 +0500

    13. Добавил списки listCheckBox для каждого абонента: BVKsh, BPS_UVKE_SM, KI, J1939. Переделал добавление вкладок с названиями обменов и их параметров для панели tabbedPane на следующий алгоритм: прохожу всех абонентов, получаю их параметры, если они есть, то добавляю вкладку и соответствующий список JCheckBox на вкладку.

--- src/main/java/ru/begletsov/frame/FrameSelectParam.java
+++ src/main/java/ru/begletsov/frame/FrameSelectParam.java
@@ -12,10 +12,12 @@ import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 import java.util.*;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Класс FrameSelectParam - окно выбора параметров
  * 1) создание класса 2) добавил объект класса Service со списком абонентов + методы добавляющие абоненты и их пар-ры
+ * 3) добавил списки listCheckBox для каждого абонента: BVKsh, BPS_UVKE_SM, KI, J1939
  * @author Sergei Begletsov
  * @since 07.08.2020
  * @version 2
@@ -23,8 +25,11 @@ import java.util.List;
 
 public class FrameSelectParam extends JFrame {
     private JTabbedPane tabbedPane;
-    private CheckBoxList cbList;
     private List<JCheckBox> listCheckBox;
+    private List<JCheckBox> listCheckBox_BVKsh;
+    private List<JCheckBox> listCheckBox_BPS_UVKE_SM;
+    private List<JCheckBox> listCheckBox_KI;
+    private List<JCheckBox> listCheckBox_J1939;
     private ListChoseParam listChoseParam;
     private Service serviceAbonentsAndParam;
 
@@ -42,8 +47,11 @@ public class FrameSelectParam extends JFrame {
         setSize(400, 400);
         setLocationByPlatform(true);
 
-        cbList = new CheckBoxList();
         listCheckBox = new ArrayList<>();
+        listCheckBox_BVKsh = new ArrayList<>();
+        listCheckBox_BPS_UVKE_SM = new ArrayList<>();
+        listCheckBox_KI = new ArrayList<>();
+        listCheckBox_J1939 = new ArrayList<>();
         listChoseParam = new ListChoseParam();
         serviceAbonentsAndParam = new Service();
         this.addAbonentsRS485();
@@ -60,26 +68,39 @@ public class FrameSelectParam extends JFrame {
         JCheckBox check10 = new JCheckBox("10");
         JCheckBox[] myList = { check1, check2, check3, check4, check5, check6, check7, check8, check9, check10};
         //JList[] myList = { check1, check2, check3, check4, check5, check6};
-        cbList.setListData(myList);
 
         //listCheckBox = (Vector) Arrays.asList(myList);
         listCheckBox = Arrays.asList(myList);
         //cbList.setListData((Vector) listCheckBox);
 
+        //Добавление абонентов и их пар-ов на панель выбора
         tabbedPane = new JTabbedPane(JTabbedPane.LEFT);
-        tabbedPane.addTab("BR_BVKsh", new JLabel("Label BR_BVKsh"));
-        tabbedPane.addTab("BR_BPS_UVKE_SM",cbList);
-        tabbedPane.addTab("BR_BR_KI", new JLabel("Label BR_BR_KI"));
-        tabbedPane.addTab("BR_ZPR_BR1", new JLabel("Label BR_ZPR_BR1"));
-        tabbedPane.addTab("BR_ZPR_BR2", new JLabel("Label BR_ZPR_BR2"));
-        tabbedPane.addTab("BR_ZPR_BR3", new JLabel("Label BR_ZPR_BR3"));
-        tabbedPane.addTab("BR_J1939", new JLabel("Label BR_J1939"));
-        tabbedPane.addTab("BR_JN2100", new JLabel("Label BR_JN2100"));
-        tabbedPane.addTab("BR_TSR_CAN", new JLabel("Label BR_TSR_CAN"));
-        tabbedPane.addTab("BR_P10x", new JLabel("Label BR_P10x"));
-        tabbedPane.addTab("BR_DIAGN", new JLabel("Label BR_DIAGN"));
-        tabbedPane.addTab("BR_CORR1", new JLabel("Label BR_CORR1"));
+        Set<Abonent> abonentSet = serviceAbonentsAndParam.getListAllAbonents();
+        for (Abonent abonent: abonentSet) {
+            String nameObmen = abonent.getNameObm();
+            //если у абонента есть параметры, то добавляю его на панель tabbedPane
+            if (serviceAbonentsAndParam.getListParamForAbonent(nameObmen).size() != 0) {
+                //получаю список пар-ров обмена для абонента
+                List<Param> listParamForAbonent = serviceAbonentsAndParam.getListParamForAbonent(nameObmen);
+                int countParam = serviceAbonentsAndParam.getListParamForAbonent(nameObmen).size();
+                JCheckBox[] massCheckBoxes = new JCheckBox[countParam];
+                for (int t = 0; t < countParam; t++) {
+                    massCheckBoxes[t] = new JCheckBox(listParamForAbonent.get(t).getNameSmall());
+                }
+                CheckBoxList checkBoxListCurrAbonent = new CheckBoxList();
+                checkBoxListCurrAbonent.setListData(massCheckBoxes);
 
+                switch (nameObmen) {
+                    case "BR_BVKsh":
+                    case "BR_KI":
+                    case "BR_J1939":
+                    case "BR_BPS_UVKE_SM":
+                        listCheckBox_BVKsh = Arrays.asList(massCheckBoxes);
+                        break;
+                }
+                tabbedPane.addTab(nameObmen, checkBoxListCurrAbonent);
+            }
+        }
         add(tabbedPane);
 
         addWindowListener(new WindowListener() {
@@ -91,6 +112,11 @@ public class FrameSelectParam extends JFrame {
             @Override
             public void windowClosing(WindowEvent e) {
                 listChoseParam.setListChoseParam_UVKE_SM(getChoseUserParam(listCheckBox));
+
+                listChoseParam.setListChoseParam_BVKsh(getChoseUserParam(listCheckBox_BVKsh));
+                listChoseParam.setListChoseParam_UVKE_SM(getChoseUserParam(listCheckBox_BPS_UVKE_SM));
+                listChoseParam.setListChoseParam_KI(getChoseUserParam(listCheckBox_KI));
+                listChoseParam.setListChoseParam_J1939(getChoseUserParam(listCheckBox_J1939));
             }
 
             @Override
@@ -124,7 +150,8 @@ public class FrameSelectParam extends JFrame {
         List rsl = new ArrayList();
         for (int ind = 0; ind < listCheckBox.size(); ind++) {
             if (listCheckBox.get(ind).isSelected()) {
-                rsl.add(ind);
+                //rsl.add(ind);
+                rsl.add(listCheckBox.get(ind).getText());
             }
         }
         return rsl;
